fa770a1b8ca03027b8b3226e3cf8f7a6848f4ad4
Pavel Ispravnikov <isp.pavel@gmail.com>
2022-08-07 14:57:48 -0400

    added skill content and new list icon

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -8,24 +8,24 @@ function Skills() {
         <div className="skills__lists">
         <ul className="skills__list">
           <li className="skills__list-item">JavaScript</li>
+          <li className="skills__list-item">NodeJS</li>
           <li className="skills__list-item">React</li>
-          <li className="skills__list-item">Python</li>
-          <li className="skills__list-item">C++</li>
-          <li className="skills__list-item">GatsbyJS</li>
+          <li className="skills__list-item">Express</li>
+          <li className="skills__list-item">MySQL</li>
         </ul>
         <ul className="skills__list">
-          <li className="skills__list-item">JavaScript</li>
-          <li className="skills__list-item">React</li>
-          <li className="skills__list-item">Python</li>
-          <li className="skills__list-item">C++</li>
+          <li className="skills__list-item">Agile Development</li>
+          <li className="skills__list-item">DOM APIs &amp; Web APIs</li>
           <li className="skills__list-item">GatsbyJS</li>
+          <li className="skills__list-item">User Authentication</li>
+          <li className="skills__list-item">OAuth</li>
         </ul>
         <ul className="skills__list">
-          <li className="skills__list-item">JavaScript</li>
-          <li className="skills__list-item">React</li>
-          <li className="skills__list-item">Python</li>
-          <li className="skills__list-item">C++</li>
-          <li className="skills__list-item">GatsbyJS</li>
+          <li className="skills__list-item">Git &amp; Github</li>
+          <li className="skills__list-item">Jest</li>
+          <li className="skills__list-item">SASS</li>
+          <li className="skills__list-item">CSS</li>
+          <li className="skills__list-item">HTML</li>
         </ul>
         </div>
         
